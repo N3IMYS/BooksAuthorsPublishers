f66fd0fd0a8f138df43837ff3d39e24deead7017
daniel <daniel@homp-W10>
2017-12-28 15:43:59 +0500

    3d Commit to CrossOrigin

--- BooksAuthorPublisher/src/main/java/telran/books/controller/BooksHandler.java
+++ BooksAuthorPublisher/src/main/java/telran/books/controller/BooksHandler.java
@@ -1,6 +1,7 @@
 package telran.books.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,37 +21,44 @@ public class BooksHandler {
 	IBooks books;
 
 	@PostMapping(IBookConstants.BOOK)
+	@CrossOrigin
 	public boolean addBook(@RequestBody Book book) {
 		return books.addBook(book);
 	}
 
 	@DeleteMapping({ IBookConstants.BOOK + "/{isbn}" })
+	@CrossOrigin
 	public boolean removeBook(@PathVariable long isbn) {
 		return books.removeBook(isbn);
 	}
 
 	@GetMapping({ IBookConstants.BOOK + "/{isbn}" })
+	@CrossOrigin
 	public Book getBook(@PathVariable long isbn) {
 		return books.getBookByISBN(isbn);
 
 	}
 
 	@GetMapping({ IBookConstants.AUTHOR + "/{name}" })
+	@CrossOrigin
 	public Iterable<Book> booksByAuthor(@PathVariable String name) {
 		return books.getBooksbyAuthor(name);
 	}
 
 	@GetMapping({ IBookConstants.PUBLISHER + "/{publisherName}" })
+	@CrossOrigin
 	public Iterable<Book> booksByPublisher(@PathVariable String publisherName) {
 		return books.getBooksByPublisher(publisherName);
 	}
 
 	@GetMapping({ IBookConstants.AUTHORS + "/{isbn}" })
+	@CrossOrigin
 	public Iterable<Author> getAuthorsByBook(@PathVariable long isbn) {
 		return books.getAuthorsByBook(isbn);
 	}
 
 	@GetMapping({ IBookConstants.PUBLISHERS+ "/{authorName}" })
+	@CrossOrigin
 	public Iterable<Publisher> getPublishersByAuthorName(@PathVariable String authorName) {
 		return books.getPublishersByAuthor(authorName);
 
